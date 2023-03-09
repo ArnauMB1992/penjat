cca6d32d384e450be2b34e3397fc2d7e2f35e750
ArnauMB <ArnauMB@DESKTOP-LF57AP7>
2023-03-09 16:45:07 +0100

    limpar pantalla

--- src/miproyecto_/Ex03_Penjat.java
+++ src/miproyecto_/Ex03_Penjat.java
@@ -4,6 +4,8 @@
  */
 package miproyecto_;
 
+import java.io.IOException;
+
 /**
  *
  * @author ArnauMB
@@ -123,7 +125,15 @@ public class Ex03_Penjat {
     }
     
     static void netejaPantalla() {
-           
+           try {
+  		if (System.getProperty("os.name").contains("Windows")) {
+    		  new ProcessBuilder("cmd", "/c", "cls").
+                          inheritIO().start().waitFor();
+  		} else {
+    	    		System.out.print("\033[H\033[2J");
+    			System.out.flush();
+  		}
+            } catch (IOException | InterruptedException ex) {}
     }
     
     
